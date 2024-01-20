63727c8f88ca4ca0bdcb60484611b1f0032a215f
Cong-Thanh Vu <vuthanh.cdt@gmail.com>
2024-01-20 16:58:45 +0800

    update object detection for project 2

---
+++ irobot/scout_ros2/scout_simulation/launch/object_tracking.launch.py
@@ -0,0 +1,73 @@
+import launch
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
+from launch_ros.actions import Node
+import os
+from pathlib import Path
+
+MY_ROBOT = os.environ.get('ROBOT', "scout_mini")
+MY_ENVIRONMENT = os.environ.get('ENV', "object_tracking")
+
+def generate_launch_description():
+    default_world_path = os.path.join(get_package_share_directory('scout_simulation'), 'worlds', MY_ENVIRONMENT + '.world')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    robot_dir = LaunchConfiguration(
+        'robot_dir',
+        default=os.path.join(get_package_share_directory('scout_simulation'),
+            'robots/'+MY_ROBOT,
+            MY_ROBOT+'.urdf'))
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+    
+    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+
+    urdf = os.path.join(get_package_share_directory('scout_simulation'), 'robots/'+MY_ROBOT+'/', MY_ROBOT+'.urdf')
+
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', MY_ROBOT, '-file', urdf,"-x", "4.487673", "-y", "1.266304", "-z", "0.100202","-R","-0.003752","-P","0.0","-Y","1.304656"],
+        output='screen',
+        )
+
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=[urdf])
+
+    gazebo = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+            ),
+            launch_arguments={
+                'world': default_world_path
+            }.items()
+        )
+    
+    config_rviz = os.path.join(
+        get_package_share_directory('scout_simulation'),
+        'rviz',
+        'scout_simulation.rviz'
+        )
+        
+    # Rviz2 node
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='scout_simulation',
+        output='screen',
+        arguments=[["-d"], [config_rviz]],
+    )
+
+    return LaunchDescription([spawn_entity, start_robot_state_publisher_cmd, gazebo])
--- project/object_tracking/README.md
+++ project/object_tracking/README.md
@@ -3,6 +3,12 @@ This package includes essential commands to understand the basic principles of a
 
 If you want to try in the simulator, make sure Gazebo is enabled by running the following command:
 ```
-ros2 launch scout_simulation object_detection.launch.py
+ros2 launch scout_simulation object_tracking.launch.py
 ```
+## Object detection
+The robot uses the YOLOv8 model for object recognition. If you wish to change the model, navigate to the ``object_detection/scripts/weights`` directory and rename the model file accordingly. To train for the custom model, please refer by [this tutorial]((project/train_model_yolov8)).
 
+```
+ros2 launch object_detection object_detection.launch.launch.py #for real robot
+ros2 launch object_detection object_detection_gazebo.launch.launch.py #for gazebo
+```
